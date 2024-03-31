f0e4a8412b0ad12d5e5debb199d0b3c6d0671be3
Nitesh <Nitesh8825kr@gmail.com>
2024-03-31 17:09:46 +0530

    some changes

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ server.use(passport.authenticate("session"));
 server.use(
   // allow to communicate with different origin domains
   cors({
-    // origin: "http://localhost:3000",
+    origin: "http://localhost:3000",
     // credentials: true,
     exposedHeaders: ["X-Total-Count"],
   })
